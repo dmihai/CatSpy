4c9e276c5b6a0b14157259d14ffa43e9f083bfa1
Mihai Doarna <dmihai@gmail.com>
2013-04-21 02:22:43 +0000

    move updated

--- python/move.py
+++ python/move.py
@@ -12,6 +12,9 @@ m2_en = 25
 m2_p1 = 23
 m2_p2 = 24
 
+power = 75
+frecv = 500
+
 io.setup(m1_en, io.OUT)
 io.setup(m1_p1, io.OUT)
 io.setup(m1_p2, io.OUT)
@@ -23,15 +26,28 @@ io.setup(m2_p2, io.OUT)
 io.output(m1_en, 1)
 io.output(m2_en, 1)
 
-#print '1' if sys.argv[1]==1 else '0';
-
 io.output(m1_p1, 1 if sys.argv[1]=='1' else 0)
 io.output(m1_p2, 1 if sys.argv[1]=='2' else 0)
 
-io.output(m2_p1, 1 if sys.argv[2]=='1' else 0)
-io.output(m2_p2, 1 if sys.argv[2]=='2' else 0)
+io.output(m2_p1, 1 if sys.argv[1]=='1' else 0)
+io.output(m2_p2, 1 if sys.argv[1]=='2' else 0)
 
-sleep(5)
+sleep(1)
+
+if(sys.argv[2] == '0')
+    sleep(4)
+
+if(sys.argv[2] == '1')
+    p = io.PWM(m1_p1 if sys.argv[1]=='1' else m1_p2, frecv)
+    p.start(power)
+    sleep(4)
+    p.stop()
+
+if(sys.argv[2] == '2')
+    p = io.PWM(m2_p1 if sys.argv[1]=='1' else m2_p2, frecv)
+    p.start(power)
+    sleep(4)
+    p.stop()
 
 io.output(m1_en, 0)
 io.output(m2_en, 0)
